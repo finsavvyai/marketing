d520d1c054d0d6304c247337de115e2595c652ce
Shachar Solomon <Shahar.solomon@teliacompany.com>
2025-03-30 01:58:26 +0300

    Add video with Git LFS

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    sourcemap: false,
   },
 });
