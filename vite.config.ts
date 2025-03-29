e5769e1099bba9ba49177e8fa73eaff1b0134663
Shachar Solomon <Shahar.solomon@teliacompany.com>
2025-03-29 21:06:22 +0300

    Github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: "./",
-  server: {
-    port: 5174,
-    host: '0.0.0.0',
-    open: false
-  }
+  base: './', // Add this line for GitHub Pages
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
